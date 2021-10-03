9aff55366b6adfb00ebbfa091bcc2a402dd6647e
LadyDarkWolf <87519139+LadyDarkWolf@users.noreply.github.com>
2021-10-03 15:11:50 +1100

    Added link to GitHub for source

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Forecast from "./Forecast";
 import Utility from "./Utility";
 import Search from "./Search";
 import Today from "./Today";
+import Source from "./Source";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Search />
         <Today />
         <Forecast />
+        <Source />
       </div>
     </div>
   );
---
+++ src/Source.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Source() {
+  return (
+    <p class="center-text">
+      Source can be found at{" "}
+      <a
+        href="https://github.com/LadyDarkWolf/reactive-weatrherboard"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub
+      </a>
+      .
+    </p>
+  );
+}
